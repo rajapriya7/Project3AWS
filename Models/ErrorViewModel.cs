9c17ba8079765d239a6284a7aba0d4abc93ac8c2
rajapriya7 <rajapriyakalimuthu@gmail.com>
2022-08-26 20:03:37 +0530

    my commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project3AWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
